311df38d219cb0ca690b5a69f40bc33b68725270
Nils Wilhelm <108529126+nwilhelmFFT@users.noreply.github.com>
2023-08-29 13:14:35 +0200

    feat(common): add method to get label for parcel

--- src/fft-api/parcel/fftParcelService.ts
+++ src/fft-api/parcel/fftParcelService.ts
@@ -3,6 +3,7 @@ import { Parcel } from '../types';
 import { FftApiClient } from '../common';
 import { Logger } from 'tslog';
 import { CustomLogger } from '../../common';
+import { ResponseType } from '../../common/httpClient/models';
 
 export class FftParcelService {
   private readonly path = 'parcels';
@@ -26,5 +27,24 @@ export class FftParcelService {
 
   public async findMultiple(ids: string[]): Promise<Parcel[]> {
     return await Promise.all(ids.map(async (i) => await this.findById(i)));
+  }
+
+  public async getLabel(parcelId: string, labelDocument: string): Promise<Buffer> {
+    try {
+      return await this.apiClient.get<Buffer>(
+        `${this.path}/${parcelId}/labels/${labelDocument}`,
+        undefined,
+        ResponseType.BLOB
+      );
+    } catch (err) {
+      const httpError = err as ResponseError;
+      this.logger.error(
+        `Could not get label ${labelDocument} for parcel with id '${parcelId}'. Failed with status ${
+          httpError.status
+        }, error: ${httpError.response ? JSON.stringify(httpError.response.body) : ''}`
+      );
+
+      throw err;
+    }
   }
 }
