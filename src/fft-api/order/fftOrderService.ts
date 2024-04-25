e30610c108f55e07e1e8e2b0bb2bf1f2e05e47f3
Arno Erpenbeck <88486704+arnoerpenbeck@users.noreply.github.com>
2024-04-25 15:12:50 +0200

    feat(CTC-125): add methods for order, pickjob, handoverjob cancellation

--- src/fft-api/order/fftOrderService.ts
+++ src/fft-api/order/fftOrderService.ts
@@ -1,5 +1,12 @@
 import { ResponseError } from 'superagent';
-import { Order, OrderForCreation, StrippedOrder, StrippedOrders } from '../types';
+import {
+  Order,
+  OrderCancelActionParameter,
+  OrderForCreation,
+  OrderUnlockActionParameter,
+  StrippedOrder,
+  StrippedOrders,
+} from '../types';
 import { FftApiClient } from '../common';
 import { Logger } from 'tslog';
 import { CustomLogger } from '../../common';
@@ -11,9 +18,9 @@ export class FftOrderService {
 
   public async create(orderForCreation: OrderForCreation): Promise<Order> {
     try {
-      const order = await this.apiClient.post<Order>('orders', { ...orderForCreation });
+      const order = await this.apiClient.post<Order>(`${this.path}`, { ...orderForCreation });
       this.logger.info(
-        `Successfully posted order with tenantOrderId '${orderForCreation.tenantOrderId}' and order id '${order.id}'`
+        `Successfully created order with tenantOrderId '${orderForCreation.tenantOrderId}' and order id '${order.id}'`
       );
 
       return order;
@@ -29,18 +36,40 @@ export class FftOrderService {
     }
   }
 
-  public async cancel(orderId: string) {
+  public async cancel(orderId: string, version: number): Promise<Order> {
     try {
-      const order = await this.apiClient.post<Order>(`orders/${orderId}/cancel`);
-      this.logger.info(`Successfully canceled order with order id '${orderId}'`);
+      const order = await this.apiClient.post<Order>(`${this.path}/${orderId}/actions`, {
+        name: OrderCancelActionParameter.NameEnum.CANCEL,
+        version,
+      });
 
       return order;
     } catch (err) {
       const httpError = err as ResponseError;
       this.logger.error(
-        `FFT cancel order for id '${orderId}' failed with status ${httpError.status}, error: ${
-          httpError.response ? JSON.stringify(httpError.response.body) : ''
-        }`
+        `Could not cancel order with id '${orderId}' and version ${version}. Failed with status ${
+          httpError.status
+        }, error: ${httpError.response ? JSON.stringify(httpError.response.body) : ''}`
+      );
+
+      throw err;
+    }
+  }
+
+  public async unlock(orderId: string, version: number): Promise<Order> {
+    try {
+      const order = await this.apiClient.post<Order>(`${this.path}/${orderId}/actions`, {
+        name: OrderUnlockActionParameter.NameEnum.UNLOCK,
+        version,
+      });
+
+      return order;
+    } catch (err) {
+      const httpError = err as ResponseError;
+      this.logger.error(
+        `Could not unlock order with id '${orderId}' and version ${version}. Failed with status ${
+          httpError.status
+        }, error: ${httpError.response ? JSON.stringify(httpError.response.body) : ''}`
       );
 
       throw err;
@@ -58,7 +87,6 @@ export class FftOrderService {
     const length = strippedOrders.orders?.length || 0;
     const firstOrder = strippedOrders.orders?.[0];
     if (!firstOrder) {
-      this.logger.info(`Did not find order with tenantOrderId '${tenantOrderId}'`);
       return undefined;
     }
 
