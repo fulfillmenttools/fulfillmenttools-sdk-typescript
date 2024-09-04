ee28685b16a75dbc975d91396956d3207d28975b
Arno Erpenbeck <88486704+arnoerpenbeck@users.noreply.github.com>
2024-09-04 17:23:35 +0200

    fix(CON-460): do not use Node.js built-in libraries

--- src/common/httpClient/serialize.ts
+++ src/common/httpClient/serialize.ts
@@ -1,4 +1,14 @@
-import { isDate } from 'util/types';
+function objectToString(o: unknown): string {
+  return Object.prototype.toString.call(o);
+}
+
+function isObject(arg: unknown): arg is object {
+  return typeof arg === 'object' && arg !== null;
+}
+
+export function isDate(date: unknown): date is Date {
+  return isObject(date) && objectToString(date) === '[object Date]';
+}
 
 export function serializeWithDatesAsIsoString(key: string, value: unknown): unknown {
   return isDate(value) ? value.toISOString() : value;
