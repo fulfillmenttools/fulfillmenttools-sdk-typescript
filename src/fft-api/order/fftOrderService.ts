e827f00e2258e4d0e21f8bbce26d8d06439e2348
Arno Erpenbeck <88486704+arnoerpenbeck@users.noreply.github.com>
2024-12-09 08:07:20 +0100

    add TODO marker

--- src/fft-api/order/fftOrderService.ts
+++ src/fft-api/order/fftOrderService.ts
@@ -31,6 +31,7 @@ export class FftOrderService {
     }
   }
 
+  // TODO cancelationReasonId
   public async cancel(orderId: string, version: number, shouldForceCancellation = false): Promise<Order> {
     try {
       const order = await this.apiClient.post<Order>(`${this.path}/${orderId}/actions`, {
