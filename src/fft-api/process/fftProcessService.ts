d5c2c994637d73add770fa885c7b64f621dfe90b
Arno Erpenbeck <88486704+arnoerpenbeck@users.noreply.github.com>
2024-12-09 11:07:14 +0100

    feat: Query process by parameters (#85)

--- src/fft-api/process/fftProcessService.ts
+++ src/fft-api/process/fftProcessService.ts
@@ -1,3 +1,4 @@
+import { ErrorType, FftSdkError, QueryParams } from '../../common';
 import { Logger } from '../../common/utils/logger';
 import { FftApiClient } from '../common';
 import { Process } from '../types';
@@ -18,4 +19,52 @@ export class FftProcessService {
       throw err;
     }
   }
+
+  public async get(parameters?: ProcessQueryParameters): Promise<Process> {
+    try {
+      const queryParams: QueryParams = {};
+
+      if (parameters?.orderRef) {
+        queryParams['orderRef'] = parameters.orderRef;
+      }
+
+      if (parameters?.tenantOrderId) {
+        queryParams['tenantOrderId'] = parameters.tenantOrderId;
+      }
+
+      if (parameters?.pickJobRef) {
+        queryParams['pickJobRef'] = parameters.pickJobRef;
+      }
+
+      if (parameters?.shipmentRef) {
+        queryParams['shipmentRef'] = parameters.shipmentRef;
+      }
+
+      if (parameters?.handoverJobRef) {
+        queryParams['handoverJobRef'] = parameters.handoverJobRef;
+      }
+
+      if (parameters?.returnRef) {
+        queryParams['returnRef'] = parameters.returnRef;
+      }
+
+      if (Object.keys(queryParams).length === 0) {
+        throw new FftSdkError({ message: 'At least one query parameter is required.', type: ErrorType.REQUEST });
+      }
+
+      return await this.apiClient.get<Process>(this.path, queryParams);
+    } catch (err) {
+      this.log.error(`Could not get process.`, err);
+      throw err;
+    }
+  }
+}
+
+export interface ProcessQueryParameters {
+  orderRef?: string;
+  tenantOrderId?: string;
+  pickJobRef?: string;
+  shipmentRef?: string;
+  handoverJobRef?: string;
+  returnRef?: string;
 }
